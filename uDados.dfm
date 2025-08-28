2d7feaa5efbdbc37a790602af5051502e9129660
IsacLim <74792919+IsacLim@users.noreply.github.com>
2025-08-28 11:48:52 -0300

    Ajuste no endereço do banco

--- uDados.dfm
+++ uDados.dfm
@@ -1,11 +1,9 @@
 object Dados: TDados
+  OnCreate = DataModuleCreate
   Height = 154
   Width = 135
   object Conn: TFDConnection
     Params.Strings = (
-      
-        'Database=D:\Usu'#225'rios\Isac\'#193'rea de Trabalho\Projeto Lanchonete\ba' +
-        'nco'
       'DriverID=SQLite')
     Left = 32
     Top = 32
--- uDados.pas
+++ uDados.pas
@@ -8,11 +8,12 @@ uses
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.SQLite,
   FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs,
   FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.FMXUI.Wait, Data.DB,
-  FireDAC.Comp.Client;
+  FireDAC.Comp.Client, FireDAC.VCLUI.Wait;
 
 type
   TDados = class(TDataModule)
     Conn: TFDConnection;
+    procedure DataModuleCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,8 +25,20 @@ var
 
 implementation
 
+uses
+  vcl.Forms;
+
 {%CLASSGROUP 'FMX.Controls.TControl'}
 
 {$R *.dfm}
+
+procedure TDados.DataModuleCreate(Sender: TObject);
+begin
+  Conn.Connected:= False;
+  Conn.Params.Clear;
+  Conn.Params.DriverID:= 'SQLite';
+  Conn.Params.Database:= ExtractFilePath(Application.ExeName) + 'banco';
+  Conn.Connected:= True;
+end;
 
 end.
